3815539a85976a7a3f1b1c3f5d7cff0f5a3fa2ff
kwojtowicz97 <kamilwojtowicz079@gmail.com>
2022-09-06 17:04:24 +0200

    Added dummy company

--- backend/data/companies.ts
+++ backend/data/companies.ts
@@ -1,14 +1,28 @@
-const companies = {
-  name: 'Apple inc.',
-  address: 'One Apple Park Way',
-  city: 'Cupertino',
-  image: '../images/Apple-logo.png',
-  user: '63068ea7db0f26ca10cb7829',
-  postalAddress: 'CA 95014',
-  phoneNumber: '(408) 996–1010',
-  country: 'USA',
-  description: 'Sample description',
-  reviews: [],
-}
+const companies = [
+  {
+    name: 'Apple inc.',
+    address: 'One Apple Park Way',
+    city: 'Cupertino',
+    image: '../images/Apple-logo.png',
+    user: '63068ea7db0f26ca10cb7829',
+    postalAddress: 'CA 95014',
+    phoneNumber: '(408) 996–1010',
+    country: 'USA',
+    description: 'Sample description',
+    reviews: [],
+  },
+  {
+    name: 'Lingaro',
+    address: 'ul. Marszałkowska 132/21',
+    city: 'Warsaw',
+    image: '../images/lingaro.png',
+    user: '63068ea7db0f26ca10cb7829',
+    postalAddress: '26-600',
+    phoneNumber: '+48 539-32-21',
+    country: 'Poland',
+    description: 'Sample description',
+    reviews: [],
+  },
+]
 
 export default companies
