421008e5615708ab3cc204a680fc32b169cec922
kwojtowicz97 <kamilwojtowicz079@gmail.com>
2022-09-07 20:28:59 +0200

    Added google Map component

---
+++ frontend/src/components/Map.tsx
@@ -0,0 +1,20 @@
+import React from 'react'
+
+interface Props {
+  city: string
+  address: string
+}
+
+const Map = ({ city, address }: Props) => {
+  return (
+    <iframe
+      width='auto'
+      height='300px'
+      style={{ border: 'none', padding: '0' }}
+      referrerPolicy='no-referrer-when-downgrade'
+      src={`https://www.google.com/maps/embed/v1/place?key=AIzaSyCGmirSsDuPqW2HuouzdFAWl4n6J2l9Krw&q=${city}+${address}`}
+    ></iframe>
+  )
+}
+
+export default Map
