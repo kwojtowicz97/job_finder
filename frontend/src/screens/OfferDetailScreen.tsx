6b7624ada055d3cc7f3f920512f82b96e1c5b2b5
kwojtowicz97 <kamilwojtowicz079@gmail.com>
2022-09-08 19:10:53 +0200

    Added sidebar and layout fixes on OfferDetailsScreen.tsx

--- frontend/src/screens/OfferDetailScreen.tsx
+++ frontend/src/screens/OfferDetailScreen.tsx
@@ -1,5 +1,5 @@
-import React, { useEffect } from 'react'
-import { Col, Row, Image, Container } from 'react-bootstrap'
+import React, { useEffect, useState } from 'react'
+import { Col, Row, Image, Container, Button } from 'react-bootstrap'
 import { useDispatch, useSelector } from 'react-redux'
 import { useParams } from 'react-router-dom'
 import { AppDispatch } from '../store'
@@ -10,10 +10,17 @@ import Message from '../components/Message'
 import Rating from '../components/Rating'
 import Map from '../components/Map'
 import { Benefits } from '../components/Benefits'
+import SaveIcon from '../components/SaveIcon'
 
 const OfferDetailScreen: React.FC = () => {
   const params = useParams()
   const dispatch = useDispatch<AppDispatch>()
+
+  const [isSaved, setIsSaved] = useState(false)
+
+  const saveClickHandler = () => {
+    setIsSaved((state) => !state)
+  }
 
   const { offer, loading, error } = useSelector(
     (state: ReduxState) => state.offerDetail
@@ -31,77 +38,135 @@ const OfferDetailScreen: React.FC = () => {
         <Message variant='danger'>{error}</Message>
       ) : (
         offer && (
-          <>
-            <Container fluid className='border rounded'>
-              <Row>
-                <Col
-                  className='border-end d-flex justify-content-center align-items-center'
-                  style={{ flex: '0 0 114px' }}
+          <Container>
+            <Row>
+              <Col className='col-8'>
+                {' '}
+                <Container fluid className='border rounded'>
+                  <Row>
+                    <Col
+                      className='border-end d-flex justify-content-center align-items-center'
+                      style={{ flex: '0 0 114px' }}
+                    >
+                      <Image
+                        src={offer.company.image}
+                        style={{ width: '80px', height: 'auto' }}
+                      ></Image>
+                    </Col>
+                    <Col className='p-3'>
+                      <h1 className='fs-3'>
+                        <b>{offer.title}</b>
+                      </h1>
+                      <div className='d-flex align-items-center'>
+                        <h2 className='d-inline m-0 fs-5'>
+                          {offer.company.name}
+                        </h2>
+                        <Rating value={offer.company.rating}></Rating>
+                      </div>
+                    </Col>
+                  </Row>
+                  <Benefits offer={offer} />
+                  <Row className='border-top'>
+                    <Map
+                      city={offer.company.city}
+                      address={offer.company.address}
+                    />
+                  </Row>
+                </Container>
+                <Container fluid className=' mt-3 border rounded'>
+                  <Row className='d-block'>
+                    <Col className='p-4 pe-0'>
+                      <span className='d-flex'>
+                        <span className='benefit-item d-flex align-items-center justify-content-center'>
+                          <i className='fa-solid fa-list-check fs-5'></i>
+                        </span>
+                        <h3 className='mb-3 ms-2 d-inline'>Responsibilities</h3>
+                      </span>
+                      <ul style={{ listStyleType: 'none', padding: '0' }}>
+                        {offer.responsibilities.map((item) => (
+                          <li className='my-2'>
+                            <span>
+                              <i className='fa-solid fa-play me-1' />
+                              <p className='d-inline'>{item}</p>
+                            </span>
+                          </li>
+                        ))}
+                      </ul>
+                    </Col>
+                    <Col className='p-4 pe-0 border-top'>
+                      <span className='d-flex'>
+                        <span className='benefit-item d-flex align-items-center justify-content-center'>
+                          <i className='fa-solid fa-id-card fs-5'></i>
+                        </span>
+                        <h3 className='mb-3 ms-2 d-inline'>Requirements</h3>
+                      </span>
+                      <ul style={{ listStyleType: 'none', padding: '0' }}>
+                        {offer.responsibilities.map((item) => (
+                          <li className='my-2'>
+                            <span>
+                              <i className='fa-solid fa-play me-1' />
+                              <p className='d-inline'>{item}</p>
+                            </span>
+                          </li>
+                        ))}
+                      </ul>
+                    </Col>
+                    <Col className='p-4 pe-0 border-top'>
+                      <span className='d-flex'>
+                        <span className='benefit-item d-flex align-items-center justify-content-center'>
+                          <i className='fa-regular fa-circle-up fs-5'></i>
+                        </span>
+                        <h3 className='mb-3 ms-2 d-inline'>We Offer</h3>
+                      </span>
+                      <ul style={{ listStyleType: 'none', padding: '0' }}>
+                        {offer.responsibilities.map((item) => (
+                          <li className='my-2'>
+                            <span>
+                              <i className='fa-solid fa-play me-1' />
+                              <p className='d-inline'>{item}</p>
+                            </span>
+                          </li>
+                        ))}
+                      </ul>
+                    </Col>
+                  </Row>
+                </Container>
+              </Col>
+              <Col>
+                <Container
+                  fluid
+                  className='border rounded position-sticky'
+                  style={{ top: '24px' }}
                 >
-                  <Image
-                    src={offer.company.image}
-                    style={{ width: '80px', height: 'auto' }}
-                  ></Image>
-                </Col>
-                <Col className='p-3'>
-                  <h1 className='fs-3'>
-                    <b>{offer.title}</b>
-                  </h1>
-                  <div className='d-flex align-items-center'>
-                    <h2 className='d-inline m-0 fs-5'>{offer.company.name}</h2>
-                    <Rating value={offer.company.rating}></Rating>
-                  </div>
-                </Col>
-              </Row>
-              <Benefits offer={offer} />
-              <Row className='border-top'>
-                <Map
-                  city={offer.company.city}
-                  address={offer.company.address}
-                />
-              </Row>
-            </Container>
-            <Container fluid className=' mt-3 border rounded'>
-              <Row>
-                <Col className='col-sm-12 col-md-6 border-end p-4 pe-0'>
-                  <span className='d-flex'>
-                    <span className='benefit-item d-flex align-items-center justify-content-center'>
-                      <i className='fa-solid fa-list-check fs-4'></i>
-                    </span>
-                    <h3 className='mb-3 ms-2 d-inline'>Responsibilities</h3>
-                  </span>
-                  <ul style={{ listStyleType: 'none', padding: '0' }}>
-                    {offer.responsibilities.map((item) => (
-                      <li className='my-4'>
-                        <span>
-                          <i className='fa-solid fa-play me-1' />
-                          <p className='d-inline'>{item}</p>
-                        </span>
-                      </li>
-                    ))}
-                  </ul>
-                </Col>
-                <Col className='col-sm-12 col-md-6 p-4 pe-0'>
-                  <span className='d-flex'>
-                    <span className='benefit-item d-flex align-items-center justify-content-center'>
-                      <i className='fa-regular fa-circle-check fs-4'></i>
-                    </span>
-                    <h3 className='mb-3 ms-2 d-inline'>Requirements</h3>
-                  </span>
-                  <ul style={{ listStyleType: 'none', padding: '0' }}>
-                    {offer.responsibilities.map((item) => (
-                      <li className='my-2'>
-                        <span>
-                          <i className='fa-solid fa-play me-1' />
-                          <p className='d-inline'>{item}</p>
-                        </span>
-                      </li>
-                    ))}
-                  </ul>
-                </Col>
-              </Row>
-            </Container>
-          </>
+                  <Row className='border-bottom'>
+                    <Button
+                      className='mx-auto p-3 my-4'
+                      variant='primary'
+                      style={{ maxWidth: '80%' }}
+                    >
+                      Apply Now
+                    </Button>
+                  </Row>
+                  <Row className='text-center'>
+                    <Col className='p-3 border-end'>
+                      <SaveIcon
+                        className='black'
+                        reverse
+                        isSaved={isSaved}
+                        onClick={saveClickHandler}
+                      />
+                    </Col>
+                    <Col className='p-3 offer-sidebar-button'>
+                      <span>
+                        <i className='fa-solid fa-print ' />
+                        <p className='d-inline'> Print</p>
+                      </span>
+                    </Col>
+                  </Row>
+                </Container>
+              </Col>
+            </Row>
+          </Container>
         )
       )}
     </>
