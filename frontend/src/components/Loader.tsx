d47213eb3524a49846b4dd1dc4feb1c236a2d39a
kwojtowicz97 <kamilwojtowicz079@gmail.com>
2022-09-09 00:46:00 +0200

    Centering Loader on the page

--- frontend/src/components/Loader.tsx
+++ frontend/src/components/Loader.tsx
@@ -1,23 +1,26 @@
-import { Spinner } from 'react-bootstrap'
+import { Container, Spinner } from 'react-bootstrap'
 
 /**
  * Loader component that will show a spinner when page is loading
  */
 const Loader = () => {
   return (
-    <Spinner
-      animation='border'
-      role='status'
-      style={{
-        width: '100px',
-        height: '100px',
-        margin: 'auto',
-        display: 'block',
-      }}
-      data-testid='spinner'
-    >
-      <span className='sr-only'>Loading...</span>
-    </Spinner>
+    <Container fluid className='my-auto'>
+      <Spinner
+        className='m-auto'
+        animation='border'
+        role='status'
+        style={{
+          width: '100px',
+          height: '100px',
+          margin: 'auto',
+          display: 'block',
+        }}
+        data-testid='spinner'
+      >
+        <span className='sr-only'>Loading...</span>
+      </Spinner>
+    </Container>
   )
 }
 
