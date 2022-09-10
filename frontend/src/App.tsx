2d65555d1e9e543d767948d7798b3dc3fe63cb79
kwojtowicz97 <kamilwojtowicz079@gmail.com>
2022-09-10 18:19:25 +0200

    Installed react-query devtools

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -4,8 +4,10 @@ import { BrowserRouter as Router, Route, Routes } from 'react-router-dom'
 import { Container } from 'react-bootstrap'
 import HomeScreen from './screens/HomeScreen'
 import OfferDetailScreen from './screens/OfferDetailScreen'
+import RegisterScreen from './screens/RegisterScreen'
 import Footer from './components/Footer'
 import { QueryClient, QueryClientProvider } from '@tanstack/react-query'
+import { ReactQueryDevtools } from '@tanstack/react-query-devtools'
 
 const queryClient = new QueryClient()
 
@@ -19,11 +21,13 @@ function App() {
             <Routes>
               <Route path='/offer/:id' element={<OfferDetailScreen />} />
               <Route path='/' element={<HomeScreen />} />
+              <Route path='/register' element={<RegisterScreen />} />
             </Routes>
           </Container>
         </main>
         <Footer />
       </Router>
+      <ReactQueryDevtools />
     </QueryClientProvider>
   )
 }
