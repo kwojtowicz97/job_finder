07917b604f3bbc5d41acc6c817d8e975c2f5f699
kwojtowicz97 <kamilwojtowicz079@gmail.com>
2022-09-10 16:09:07 +0200

    Installed react-query and added client to the App

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -5,21 +5,26 @@ import { Container } from 'react-bootstrap'
 import HomeScreen from './screens/HomeScreen'
 import OfferDetailScreen from './screens/OfferDetailScreen'
 import Footer from './components/Footer'
+import { QueryClient, QueryClientProvider } from '@tanstack/react-query'
+
+const queryClient = new QueryClient()
 
 function App() {
   return (
-    <Router>
-      <Header />
-      <main className='py-3 my-auto'>
-        <Container fluid='lg' className='cnt'>
-          <Routes>
-            <Route path='/offer/:id' element={<OfferDetailScreen />} />
-            <Route path='/' element={<HomeScreen />} />
-          </Routes>
-        </Container>
-      </main>
-      <Footer />
-    </Router>
+    <QueryClientProvider client={queryClient}>
+      <Router>
+        <Header />
+        <main className='py-3 my-auto'>
+          <Container fluid='lg' className='cnt'>
+            <Routes>
+              <Route path='/offer/:id' element={<OfferDetailScreen />} />
+              <Route path='/' element={<HomeScreen />} />
+            </Routes>
+          </Container>
+        </main>
+        <Footer />
+      </Router>
+    </QueryClientProvider>
   )
 }
 
