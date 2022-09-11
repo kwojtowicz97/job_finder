5e84b685fc2af49418ef04af966d020b401439b6
kwojtowicz97 <kamilwojtowicz079@gmail.com>
2022-09-11 12:50:00 +0200

    Added userContextProvider

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,4 +1,4 @@
-import React from 'react'
+import React, { createContext, Dispatch, SetStateAction, useState } from 'react'
 import Header from './components/Header'
 import { BrowserRouter as Router, Route, Routes } from 'react-router-dom'
 import { Container } from 'react-bootstrap'
@@ -9,28 +9,42 @@ import Footer from './components/Footer'
 import { QueryClient, QueryClientProvider } from '@tanstack/react-query'
 import { ReactQueryDevtools } from '@tanstack/react-query-devtools'
 import { ToastMessage } from './components/ToastMessage'
+import { UserInfo } from './types/User'
 
 const queryClient = new QueryClient()
 
+export type UserContextType = {
+  userInfo: UserInfo | null
+  setUserInfo: Dispatch<SetStateAction<UserInfo | null>> | null
+}
+
+export const userContext = createContext<UserContextType>({
+  userInfo: null,
+  setUserInfo: null,
+})
+
 function App() {
+  const [userInfo, setUserInfo] = useState<UserInfo | null>(null)
   return (
-    <QueryClientProvider client={queryClient}>
-      <Router>
-        <Header />
-        <main className='py-3'>
-          <Container fluid='lg' className='cnt'>
-            <Routes>
-              <Route path='/offer/:id' element={<OfferDetailScreen />} />
-              <Route path='/' element={<HomeScreen />} />
-              <Route path='/register' element={<RegisterScreen />} />
-            </Routes>
-          </Container>
-        </main>
-        <Footer />
-      </Router>
-      <ToastMessage />
-      <ReactQueryDevtools />
-    </QueryClientProvider>
+    <userContext.Provider value={{ userInfo, setUserInfo }}>
+      <QueryClientProvider client={queryClient}>
+        <Router>
+          <Header />
+          <main className='py-3'>
+            <Container fluid='lg' className='cnt'>
+              <Routes>
+                <Route path='/offer/:id' element={<OfferDetailScreen />} />
+                <Route path='/' element={<HomeScreen />} />
+                <Route path='/register' element={<RegisterScreen />} />
+              </Routes>
+            </Container>
+          </main>
+          <Footer />
+        </Router>
+        <ToastMessage />
+        <ReactQueryDevtools />
+      </QueryClientProvider>
+    </userContext.Provider>
   )
 }
 
