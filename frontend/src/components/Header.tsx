64d18f608e017b4cc115c1230914db8923549a48
kwojtowicz97 <kamilwojtowicz079@gmail.com>
2022-09-11 15:16:19 +0200

    Dispaly current user name in the header

--- frontend/src/components/Header.tsx
+++ frontend/src/components/Header.tsx
@@ -1,8 +1,11 @@
 import React from 'react'
+import { useContext } from 'react'
 import { Container, Nav, Navbar } from 'react-bootstrap'
 import { LinkContainer } from 'react-router-bootstrap'
+import { userContext } from '../App'
 
 const Header = () => {
+  const { userInfo } = useContext(userContext)
   return (
     <header>
       <Navbar bg='ligth' expand='lg'>
@@ -19,7 +22,8 @@ const Header = () => {
             <Nav className='ms-auto'>
               <LinkContainer to='/profile'>
                 <Nav.Link>
-                  <i className='fa-solid fa-user'></i> Profile
+                  <i className='fa-solid fa-user'></i>{' '}
+                  {userInfo ? userInfo.name : 'Profile'}
                 </Nav.Link>
               </LinkContainer>
             </Nav>
