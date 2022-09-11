450ab2b998ee85d35402532d03243b17ae3b3abf
kwojtowicz97 <kamilwojtowicz079@gmail.com>
2022-09-11 15:16:33 +0200

    Added ToastMessage.jsx

--- frontend/src/components/ToastMessage.tsx
+++ frontend/src/components/ToastMessage.tsx
@@ -1,15 +1,34 @@
-import React from 'react'
+import React, { useContext, useEffect, useState } from 'react'
 import { Toast, ToastContainer } from 'react-bootstrap'
+import { toastContext } from '../App'
 
 export const ToastMessage = () => {
-  return (
+  const { toast, setToast } = useContext(toastContext)
+  const [show, setShow] = useState(false)
+
+  setTimeout(() => setShow(true), 500)
+
+  useEffect(() => {
+    return setShow(false)
+  }, [])
+
+  return show ? (
     <ToastContainer position='bottom-end' className='position-fixed pe-3'>
-      <Toast bg='secondary' show={true}>
+      <Toast
+        onClose={() => setToast && setToast({ trigger: false })}
+        show={toast.trigger}
+        delay={5000}
+        animation
+        autohide
+      >
         <Toast.Header>
-          <strong>Hello Kamil!</strong>
+          <strong className='me-auto'>Bootstrap</strong>
+          <small>11 mins ago</small>
         </Toast.Header>
-        <Toast.Body>Your account has beed created!</Toast.Body>
+        <Toast.Body>Woohoo, you're reading this text in a Toast!</Toast.Body>
       </Toast>
     </ToastContainer>
+  ) : (
+    <></>
   )
 }
