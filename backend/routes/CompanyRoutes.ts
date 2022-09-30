69f258b41491b1f163152a9c7501603fb7b80bc7
kwojtowicz97 <kamilwojtowicz079@gmail.com>
2022-09-30 21:20:52 +0200

    Added updateCompany route in backend

--- backend/routes/CompanyRoutes.ts
+++ backend/routes/CompanyRoutes.ts
@@ -4,10 +4,11 @@ const router = express.Router()
 import {
   createNewCompany,
   getCompanyById,
+  updateCompany,
 } from '../controllers/CompanyController'
 import { protect } from '../middleware/authHandler'
 
-router.route('/:id').get(getCompanyById)
+router.route('/:id').get(getCompanyById).put(protect, updateCompany)
 router.route('/').post(protect, createNewCompany)
 
 export default router
