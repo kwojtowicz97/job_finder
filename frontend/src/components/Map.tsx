a505294dfbe80525b05645b95052f5c0b3c44e81
kwojtowicz97 <kamilwojtowicz079@gmail.com>
2022-09-30 21:22:37 +0200

    Changed style on Map.tsx

--- frontend/src/components/Map.tsx
+++ frontend/src/components/Map.tsx
@@ -3,14 +3,16 @@ import React from 'react'
 interface Props {
   city: string
   address: string
+  shadow?: boolean
 }
 
-const Map = ({ city, address }: Props) => {
+const Map = ({ city, address, shadow }: Props) => {
   return (
     <iframe
       title='map'
-      width='auto'
+      width='100%'
       height='300px'
+      className={`${shadow && 'shadow-lg'}`}
       style={{ border: 'none', padding: '0' }}
       referrerPolicy='no-referrer-when-downgrade'
       src={`https://www.google.com/maps/embed/v1/place?key=AIzaSyCGmirSsDuPqW2HuouzdFAWl4n6J2l9Krw&q=${city}+${address}`}
