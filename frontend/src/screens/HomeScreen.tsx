b30d4e0aa031b25c1622298e7818a18096171d69
kwojtowicz97 <kamilwojtowicz079@gmail.com>
2022-09-30 21:23:49 +0200

    Added SearchBar to HomeScreen

--- frontend/src/screens/HomeScreen.tsx
+++ frontend/src/screens/HomeScreen.tsx
@@ -1,4 +1,4 @@
-import React from 'react'
+import React, { useEffect, useRef, useState } from 'react'
 import { ListGroup } from 'react-bootstrap'
 import JobOffer from '../components/JobOffer'
 import Loader from '../components/Loader'
@@ -7,6 +7,8 @@ import { useQuery } from '@tanstack/react-query'
 import axios from 'axios'
 import { Offer } from '../types'
 import { errorHandler } from '../utils/errorHandler'
+import SearchBar from '../components/SearchBar'
+import ReactDOM from 'react-dom'
 
 interface ListOffersResponse {
   offers: Offer[]
@@ -14,13 +16,24 @@ interface ListOffersResponse {
   pages: number
 }
 
-const listOffers = async () => {
-  const { data } = await axios.get('/api/offers')
-  return data
+interface Props {
+  portalContainer: React.RefObject<HTMLElement>
 }
 
-const HomeScreen: React.FC = () => {
-  const { data, isLoading, isError, error } = useQuery<
+const HomeScreen = ({ portalContainer }: Props) => {
+  const [position, setPosition] = useState<string | undefined>('')
+  const [location, setLocation] = useState<string | undefined>('')
+
+  console.log('rerender')
+
+  const listOffers = async () => {
+    const { data } = await axios.get(
+      `/api/offers/?position=${position}&location=${location}`
+    )
+    return data
+  }
+
+  const { data, isLoading, isError, error, refetch } = useQuery<
     ListOffersResponse,
     Error
   >(['listOffers'], listOffers)
@@ -33,6 +46,13 @@ const HomeScreen: React.FC = () => {
         <Message variant='danger'>{errorHandler(error)}</Message>
       ) : (
         <>
+          {ReactDOM.createPortal(
+            <SearchBar
+              searchBarProps={{ position, location, setPosition, setLocation }}
+              refetch={refetch}
+            />,
+            portalContainer.current!
+          )}
           <h2>Newest Job Offers</h2>
           <ListGroup variant='flush'>
             <ListGroup.Item>
