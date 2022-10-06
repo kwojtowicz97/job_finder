9ac1fc2cb4d1e58a25bb5de591d542cf7643835d
kwojtowicz97 <kamilwojtowicz079@gmail.com>
2022-10-06 15:58:47 +0200

    Added getAllCompaies controller

--- backend/routes/CompanyRoutes.ts
+++ backend/routes/CompanyRoutes.ts
@@ -3,12 +3,13 @@ const router = express.Router()
 
 import {
   createNewCompany,
+  getAllCompanies,
   getCompanyById,
   updateCompany,
 } from '../controllers/CompanyController'
 import { protect } from '../middleware/authHandler'
 
 router.route('/:id').get(getCompanyById).put(protect, updateCompany)
-router.route('/').post(protect, createNewCompany)
+router.route('/').post(protect, createNewCompany).get(getAllCompanies)
 
 export default router
