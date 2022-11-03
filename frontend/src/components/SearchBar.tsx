34397f5a37f0a7390aa186cc9db91baaba53154e
kwojtowicz97 <kamilwojtowicz079@gmail.com>
2022-11-03 18:18:48 +0100

    Added responsive SearchBar

--- frontend/src/components/SearchBar.tsx
+++ frontend/src/components/SearchBar.tsx
@@ -1,5 +1,5 @@
 import React, { FormEvent, useState } from 'react'
-import { Button, Col, Form, Row } from 'react-bootstrap'
+import { Button, Col, Form, Offcanvas, Row } from 'react-bootstrap'
 import { useNavigate } from 'react-router-dom'
 
 interface Props {
@@ -15,6 +15,7 @@ interface Props {
 const SearchBar = ({ searchBarProps, refetch }: Props) => {
   const navigate = useNavigate()
   const { position, location, setPosition, setLocation } = searchBarProps
+  const [show, setShow] = useState(false)
   const submitHandler = (e: FormEvent) => {
     e.preventDefault()
     if (position || location)
@@ -22,38 +23,85 @@ const SearchBar = ({ searchBarProps, refetch }: Props) => {
     refetch()
   }
 
+  const offcanvasToggleHandler = () => {
+    setShow((prev) => !prev)
+  }
+
   return (
-    <Form
-      onSubmit={submitHandler}
-      className='d-flex py-5 px-3 shadow-sm mb-3 d-none d-lg-block'
-    >
-      <Row className='w-100 d-flex align-items-center'>
-        <Col className='col-2'>
-          <h2 className='text-end fs-2'>
-            <b>Search</b>
-          </h2>
-        </Col>
-        <Col className='col-5'>
-          <Form.Control
-            type='text'
-            placeholder='positions, companies, keywords'
-            value={position}
-            onChange={(e) => setPosition(e.target.value)}
-          />
-        </Col>
-        <Col className='col-4'>
-          <Form.Control
-            type='text'
-            placeholder='city, country'
-            value={location}
-            onChange={(e) => setLocation(e.target.value)}
-          />
-        </Col>
-        <Col className='col-1'>
-          <Button type='submit'>Search</Button>
-        </Col>
-      </Row>
-    </Form>
+    <>
+      <i
+        onClick={offcanvasToggleHandler}
+        style={{
+          bottom: '30px',
+          right: '30px',
+          borderRadius: '50%',
+          zIndex: 100,
+          background: '#000cde',
+          width: '50px',
+          height: '50px',
+          color: 'white',
+        }}
+        className='d-flex fs-5 justify-content-center align-items-center fa-solid fa-magnifying-glass position-fixed shadow-sm d-lg-none d-block'
+      ></i>
+      <Offcanvas placement='bottom' show={show} onHide={offcanvasToggleHandler}>
+        <Offcanvas.Header className='pb-0' closeButton>
+          <Offcanvas.Title>Search</Offcanvas.Title>
+        </Offcanvas.Header>
+        <Offcanvas.Body>
+          <Form onSubmit={submitHandler}>
+            <Form.Control
+              className='my-1'
+              type='text'
+              placeholder='positions, companies, keywords'
+              value={position}
+              onChange={(e) => setPosition(e.target.value)}
+            />
+            <Form.Control
+              className='my-1'
+              type='text'
+              placeholder='city, country'
+              value={location}
+              onChange={(e) => setLocation(e.target.value)}
+            />
+            <Button className='d-block mx-auto mt-3' type='submit'>
+              Search
+            </Button>
+          </Form>
+        </Offcanvas.Body>
+      </Offcanvas>
+      <Form
+        onSubmit={submitHandler}
+        style={{ maxWidth: '2000px' }}
+        className='mx-auto d-flex py-5 px-3 shadow-sm mb-3 d-none d-lg-block'
+      >
+        <Row className='w-100 d-flex align-items-center'>
+          <Col className='col-2'>
+            <h2 className='text-end fs-2'>
+              <b>Search</b>
+            </h2>
+          </Col>
+          <Col className='col-5'>
+            <Form.Control
+              type='text'
+              placeholder='positions, companies, keywords'
+              value={position}
+              onChange={(e) => setPosition(e.target.value)}
+            />
+          </Col>
+          <Col className='col-4'>
+            <Form.Control
+              type='text'
+              placeholder='city, country'
+              value={location}
+              onChange={(e) => setLocation(e.target.value)}
+            />
+          </Col>
+          <Col className='col-1'>
+            <Button type='submit'>Search</Button>
+          </Col>
+        </Row>
+      </Form>
+    </>
   )
 }
 
