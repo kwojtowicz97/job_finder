c67a796d58637920c714071feeac1b0df2f05147
kwojtowicz97 <kamilwojtowicz079@gmail.com>
2022-11-16 18:12:47 +0100

    Removed commented code

--- frontend/src/screens/OfferDetailScreen.tsx
+++ frontend/src/screens/OfferDetailScreen.tsx
@@ -78,24 +78,6 @@ const OfferDetailScreen: React.FC = () => {
 
   const ref = useRef(null)
 
-  function createPdf() {
-    // const mywindow = window.open('', 'PRINT', 'height=400,width=600')!
-
-    // mywindow.document.write('<html><head><title>' + document.title + '</title>')
-    // mywindow.document.write('</head><body >')
-    // mywindow.document.write('<h1>' + document.title + '</h1>')
-    // mywindow.document.write(document.getElementById(ref.current!)!.innerHTML)
-    // mywindow.document.write('</body></html>')
-
-    // mywindow.document.close() // necessary for IE >= 10
-    // mywindow.focus() // necessary for IE >= 10*/
-
-    // mywindow.print()
-    // mywindow.close()
-
-    return true
-  }
-
   return (
     <>
       {isLoading ? (
